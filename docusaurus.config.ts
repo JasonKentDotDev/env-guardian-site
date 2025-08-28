bbff16ef9741c40c50a2b82aeb9fa6482fe4275b
Jason Kent <jasonbkent@gmail.com>
2025-08-28 17:18:57 -0600

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -68,7 +68,7 @@ const config: Config = {
       items: [
         {
           type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
+          sidebarId: 'documentationSidebar',
           position: 'left',
           label: 'Docs',
         },
